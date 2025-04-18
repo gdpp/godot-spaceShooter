7420857a417ab48a342c5083ddfaa36dab1d03d2
Gustavo Perez <gustavo.perez.231191@gmail.com>
2025-04-18 23:03:11 +0200

    update project

--- scenes/level.gd
+++ scenes/level.gd
@@ -1,4 +1,11 @@
 extends Node2D
 
-func _on_timer_timeout() -> void:
-	print("Meteor")
+# 1. Load the scene
+var meteor_scene: PackedScene = load("res://scenes/meteor.tscn")
+
+func _on_meteor_timer_timeout() -> void:
+	# 2. Create an instace
+	var meteor = meteor_scene.instantiate()
+	
+	# 3. Attach the node to the scene tree
+	$Meteors.add_child(meteor)
